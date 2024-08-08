cd3505ad75cb9c00800004bae1c5ca2d50068287
Erdi Maden <erdi.maden@coinbase.com>
2024-08-08 15:40:17 -0500

    Adding Smart Wallet

--- app/config.ts
+++ app/config.ts
@@ -1,9 +1,15 @@
 import { createConfig, http } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
+import { coinbaseWallet } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [baseSepolia],
-  connectors: [],
+  connectors: [
+    coinbaseWallet({
+      appName: 'AI Feedback Tool',
+      preference: 'smartWalletOnly',
+    }),
+  ],
   transports: {
     [baseSepolia.id]: http(),
   },
